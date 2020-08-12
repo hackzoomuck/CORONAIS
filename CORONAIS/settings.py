e561c03fb19a2636c367a8c982a806a4e44c00dd
hackzoomuck <xaq03061@gmail.com>
2020-08-12 10:25:22 +0900

    settings.py databases

--- CORONAIS/settings.py
+++ CORONAIS/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'CORONAIS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db', # DB명
+        'USER': 'python', # 데이터베이스 계정
+        'PASSWORD':'python', # 계정 비밀번호
+        'HOST':'localhost', # 데이테베이스 IP
+        'PORT':'3306', # 데이터베이스 port
+     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
