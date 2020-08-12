3eb973d07e1afdf591684a99a7683fb971c43301
resourcesaga <19742930+resourceSaga@users.noreply.github.com>
2020-08-12 14:53:38 +0900

    Revert "venv file byebye"
    This reverts commit 11648efa9a918ee463bf288c9e983d35b4fb841e.

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\mypython\CORONAIS\venv\Scripts\python.exe
+#!C:\python-workspace\CORONAIS\venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
