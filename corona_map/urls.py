481ba863855bae3e3c8cacc660e4d14f4dbf9e1e
hackzoomuck <xaq03061@gmail.com>
2020-08-14 18:19:51 +0900

    add lee branch - main map page

--- corona_map/urls.py
+++ corona_map/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     # loacalhost:8080/
-    path('', views.coIs_home, name='coIs_home')
+    path('', views.coIs_home, name='coIs_home'),
+    #path('day-status/', views.day_status, name='day_status'),
+    path('seoul/', views.seoul, name='seoul'),
+    path('sidoinfo-state/', views.sidoinfo_state, name='sidoapi'),
+    path('folium_page/', views.folium_page, name='folium')
 ]
--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\python-workspace\CORONAIS\venv\Scripts\python.exe
+#!C:\mypython\CORONAIS\venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
