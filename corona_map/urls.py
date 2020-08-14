c6fd85288b7cb8de3ac2a64b98cb2682407d097d
resourcesaga <19742930+resourceSaga@users.noreply.github.com>
2020-08-14 15:58:45 +0900

    맵에 확진자수 뿌리기 테스트 완료

--- corona_map/urls.py
+++ corona_map/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     # loacalhost:8080/
     path('', views.coIs_home, name='coIs_home'),
-    path('sidoinfo-state/', views.sidoinfo_state, name='sidoinfo_state')
+    path('sidoinfo-state/', views.sidoinfo_state, name='sidoinfo_state'),
+    path('folium-page/', views.folium_page, name='folium-page')
+
 ]
--- corona_map/views.py
+++ corona_map/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import unquote
-
+import folium
+import pandas as pd
+import json
 # Create your views here.
-# 수녕, 서율, 지은
-sido_serviceKey = ['0','hFxBvUwCFBcRvWK6wJdgZXgFmjnogBAgCMQ%2BWfZmCQngtc%2FkNb%2FvVqfS2ouV%2BxKMAbEbE94ZYhW3m6A3hxKyig%3D%3D','2']
 
-def coIs_home(request):
-    return render(request, 'corona_map/coIs_home.html')
 
-def sidoinfo_state(request):
+def folium_page(request):
+    # 수녕, 서율, 지은
+    sido_serviceKey = ['0',
+                       'hFxBvUwCFBcRvWK6wJdgZXgFmjnogBAgCMQ%2BWfZmCQngtc%2FkNb%2FvVqfS2ouV%2BxKMAbEbE94ZYhW3m6A3hxKyig%3D%3D',
+                       '2']
+
     url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson'
     SERVICE_KEY = unquote(sido_serviceKey[1])
 
@@ -22,14 +25,102 @@ def sidoinfo_state(request):
         'endCreateDt': 20200811
     }
 
-
     res = requests.get(url, params=params)
+
     if res.status_code == 200:
         html = res.text
         soup = BeautifulSoup(html, 'html.parser')
-        print('#################################')
-        print(soup)
-        return render(request, 'corona_map/sidoinfo_state.html')
+        gubun_tag = soup.select('item gubun')
+        defcnt_tag = soup.select('item defcnt')
+        gubun = []
+        defcnt = []
+        for gu in gubun_tag:
+            gubun.append(gu.text)
+        for de in defcnt_tag:
+            defcnt.append(de.text)
+
+        soup_sido_data_list = dict(zip(gubun, defcnt))
+
+    geo_sido_data = 'corona_map/static/json_data/korea_sido.json'
+
+    with open(geo_sido_data, "r", encoding="utf8") as f:
+        contents = f.read()
+        json_data = json.loads(contents)
+
+    data_df = pd.DataFrame(columns=['시', '확진자'])
+
+    sido_data_list = []
+
+    for k, v in soup_sido_data_list.items():
+        sido_data = {}
+        sido_data['시'] = k
+        sido_data['확진자'] = int(v)
+        
+        sido_data_list.append(sido_data)
+
+    sido_data_list.pop(0)
+    sido_data_list.pop()
+    # print(sido_data_list)
+    for sido_data in sido_data_list:
+        series_obj = pd.Series(sido_data)
+        data_df = data_df.append(series_obj, ignore_index=True)
+
+    # 서울시 중심부의 위도, 경도
+    seoul_center = [36.3, 127.8]
+
+    # 맵이 center 에 위치하고, zoom 레벨은 7로 시작하는 맵 m
+    m = folium.Map(location=seoul_center, zoom_start=6)
+
+    # Choropleth 레이어를 만들고, 맵 m에 추가
+    folium.Choropleth(
+        geo_data=json_data,
+        data=data_df,
+        columns=('시', '확진자'),
+        key_on='feature.properties.CTP_KOR_NM',
+        fill_color='BuPu',
+        legend_name='확진자',).add_to(m)
+    m = m._repr_html_()  # updated
+
+    context = {'mapdata': m}
+    return render(request, 'corona_map/folium_page.html', context)
+
+def coIs_home(request):
+    return render(request, 'corona_map/coIs_home.html')
+
+def sidoinfo_state(request):
+    # 수녕, 서율, 지은
+    sido_serviceKey = ['0',
+                       'hFxBvUwCFBcRvWK6wJdgZXgFmjnogBAgCMQ%2BWfZmCQngtc%2FkNb%2FvVqfS2ouV%2BxKMAbEbE94ZYhW3m6A3hxKyig%3D%3D',
+                       '2']
+
+    url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson'
+    SERVICE_KEY = unquote(sido_serviceKey[1])
+
+    params = {
+        'serviceKey': SERVICE_KEY,
+        'pageNo': 1,
+        'numOfRows': 1,
+        'startCreateDt': 20200811,
+        'endCreateDt': 20200811
+    }
+
+    res = requests.get(url, params=params)
+
+    if res.status_code == 200:
+        html = res.text
+        soup = BeautifulSoup(html, 'html.parser')
+        gubun_tag = soup.select('item gubun')
+        defcnt_tag = soup.select('item defcnt')
+        gubun = []
+        defcnt = []
+        for gu in gubun_tag:
+            gubun.append(gu.text)
+        for de in defcnt_tag:
+            defcnt.append(de.text)
+
+        sido = list(zip(gubun, defcnt))
+
+        return render(request, 'corona_map/sidoinfo_state.html', {'soup_data':sido})
 
     else:
         return render(request, 'corona_map/coIs_home.html')
