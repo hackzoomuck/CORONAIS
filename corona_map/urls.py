cab485529b3cbf21d7c779047379c164e0e1f48e
sunyeongchoi <sn0716@naver.com>
2020-08-14 18:27:02 +0900

    django chart.js making

--- corona_map/urls.py
+++ corona_map/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+from django.conf.urls import url
+
 
 urlpatterns = [
     # loacalhost:8080/
-    path('', views.coIs_home, name='coIs_home')
+    path('', views.coIs_home, name='coIs_home'),
+    path('chart-bar/', views.chart_bar, name='chart-bar'),
 ]
